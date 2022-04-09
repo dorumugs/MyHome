524e4dad9a924c7803ff855c680bf6fa9b510b69
dorumugs <dorumugs@gmail.com>
2022-04-09 21:19:29 +0900

    #2022-04-08 Fixed tx > price_pre > 날짜 Lambda

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logger.info("OS Version -- " + str(platform()))
 
 if __name__ == "__main__":
     my_home = MyHome(logger)
-    # my_home.get_curl_data() # 데이터 가져오기
-    # my_home.real_estate() # 실거래, 전세가 가져오기
-    # my_home.real_estate_pre() # 데이터 전처리 하기
+    my_home.get_curl_data() # 데이터 가져오기
+    my_home.real_estate() # 실거래, 전세가 가져오기
+    my_home.real_estate_pre() # 데이터 전처리 하기
     my_home.export_elasticsearch()
